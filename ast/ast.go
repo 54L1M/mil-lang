77318131ff7626a6d17303990a57ac79bb55c993
54L1M <themhsalimi@gmail.com>
2024-01-30 23:49:08 +0330

    add ast for expressions

--- ast/ast.go
+++ ast/ast.go
@@ -1,11 +1,14 @@
 package ast
 
 import (
+	"bytes"
+
 	"github.com/54L1m/mil-lang/token"
 )
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -30,6 +33,16 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
 type Identifier struct {
 	Token token.Token
 	Value string
@@ -39,6 +52,7 @@ func (i *Identifier) expressionNode() {}
 func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
 }
+func (i *Identifier) String() string { return i.Value }
 
 type VarStatement struct {
 	Token token.Token
@@ -46,9 +60,41 @@ type VarStatement struct {
 	Value Expression
 }
 
-func (ls *VarStatement) statementNode() {}
-func (ls *VarStatement) TokenLiteral() string {
-	return ls.Token.Literal
+func (vr *VarStatement) statementNode() {}
+func (vr *VarStatement) TokenLiteral() string {
+	return vr.Token.Literal
+}
+
+func (vr *VarStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(vr.TokenLiteral() + " ")
+	out.WriteString(vr.Name.String())
+	out.WriteString(" = ")
+
+	if vr.Value != nil {
+		out.WriteString(vr.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
 }
 
 type ReturnStatement struct {
@@ -59,4 +105,18 @@ type ReturnStatement struct {
 func (rs *ReturnStatement) statementNode() {}
 func (rs *ReturnStatement) TokenLiteral() string {
 	return rs.Token.Literal
+}
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
 }
